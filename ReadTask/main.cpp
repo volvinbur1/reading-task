57cf5c1115c7a6cd6d3d61f9bcbbea9a8d47de4e
Volodymyr Burdeinyi <44869508+volvinbur1@users.noreply.github.com>
2020-01-31 00:47:01 +0200

    Add files via upload

--- ReadTask/main.cpp
+++ ReadTask/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <memory>
-#include <fstream>
+//#include <fstream>
 #include <string>
 #include <thread>
 #include <vector>
@@ -8,9 +8,12 @@
 #include <ctime>
 #include "Config.h"
 
+#pragma warning(disable : 4996)
+
 using namespace std;
 
 bool reading_is_finished = false;
+uint64_t number_of_bits = 0;
 list<string> content;
 
 void read_file_content(Config* config) {
@@ -18,15 +21,32 @@ void read_file_content(Config* config) {
 		content.push_back("__" + name + "__");
 
 		{
-			unique_ptr<ifstream> input(new ifstream);// = make_unique<ifstream>();
+			auto stream = unique_ptr<FILE, decltype(&fclose)>(fopen(name.c_str(), "rb"), &fclose);
+			
+			if (stream != NULL) {
+				fseek(stream.get(), 0, SEEK_END);
+				uint64_t size = ftell(stream.get());
+				rewind(stream.get());
 
-			input->open(config->path + name);
+				//cout << size << endl;
 
-			if (input->is_open()) {
-				string line;
-				while (getline(*input, line))
-					content.push_back(line);
+				while (ftell(stream.get()) < size) {
+					uint32_t bytes;
+					fread(&bytes, sizeof(uint32_t), 1, stream.get());
+					content.push_back(to_string(bytes));
+					fseek(stream.get(), sizeof(uint32_t), SEEK_CUR);
+					number_of_bits += sizeof(uint32_t) * 8;
+				}
 			}
+			//unique_ptr<ifstream> input(new ifstream);// = make_unique<ifstream>();
+
+			//input->open(config->path + name);
+
+			//if (input->is_open()) {
+			//	string line;
+			//	while (getline(*input, line))
+			//		content.push_back(line);
+			//}
 		}
 	}
 	reading_is_finished = true;
@@ -63,7 +83,10 @@ int main() {
 		cout << str << endl;
 	}
 
-	cout << "Whole reading took " << (end_time - start_time) / CLOCKS_PER_SEC << " seconds" << endl;
+	clock_t tooked_time = end_time - start_time;
+
+	cout << "Whole reading took " << (double) tooked_time / CLOCKS_PER_SEC << " seconds" << endl;
+	cout << "Speed of reading " << (double) number_of_bits / tooked_time << " bits per clock" << endl;
 
 	system("pause");
 
