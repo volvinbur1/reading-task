c1cf2b034d1767da908d6543c6fea7c26f574bc3
Volodymyr Burdeinyi <44869508+volvinbur1@users.noreply.github.com>
2020-02-02 19:42:10 +0200

    Add files via upload

--- ReadTask/Config.cpp
+++ ReadTask/Config.cpp
@@ -3,14 +3,20 @@
 #include <iostream>
 
 Config::Config(string _path) {
-	const auto data = toml::parse(_path);
+	try {
+		const auto data = toml::parse(_path);
 
-	title = toml::find<string>(data, "title");
-	path = toml::find<string>(data, "file", "path");
-	file_names = toml::find<vector<string>>(data, "file", "names");
-	speed = toml::find<float>(data, "settings", "speed");
-
+		title = toml::find<string>(data, "title");
+		path = toml::find<string>(data, "file", "path");
+		file_names = toml::find<vector<string>>(data, "file", "names");
+		speed = toml::find<float>(data, "settings", "speed");
+		//toml::format_error();
+	}
+	catch (const toml::exception& e) {
+		cout << e.what() << endl;
+	}
 	_speed_convert();
+	_path_corect();
 }
 
 void Config::print_all() {
@@ -27,4 +33,9 @@ void Config::print_all() {
 void Config::_speed_convert() {
 	if (speed != -1)
 		speed *= 1024.f;
+}
+
+void Config::_path_corect() {
+	if (path != "")
+		path += "\\";
 }
